e935acd914fdcd47e0c9ae140740add1d0efb5df
SM SAIF <smsaif767@gmail.com>
2024-06-10 00:09:11 +0530

    responsive update

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ import { FaArrowRight } from 'react-icons/fa'
 function Hero() {
   return (
     <div className="flex sm:flex-row flex-col content-center   mt-1 bg-transparent">
-      <div className="leftContainer flex flex-col sm:justify-center h-[650px] sm:h-[600px] content-center px-16 pt-4 sm:w-7/12 w-full bg-gray-100  ">
+      <div className="leftContainer flex flex-col sm:justify-center  sm:h-[600px] content-center px-16 pt-4 sm:w-7/12 w-full bg-gray-100  ">
         <h3 className="font-bold font-sans">Discount up to 20%</h3>
         <div className="heading lg:w-[35rem]">
           <h1 className="text-5xl inline-block ">
@@ -21,13 +21,13 @@ function Hero() {
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus
             delectus dolor quae iste optio enim natus, fuga rerum vel dolore.
           </p>
-          <div className="container flex flex-col sm:flex-row mr-4 lg:gap-32 mt-8">
-            <button className="bg-green-700 rounded-full p-1 md:px-4 text-gray-200 h-12 lg:h-8 flex text-center justify-center items-center">
+          <div className="container flex flex-col sm:flex-row mr-4 items-center  lg:gap-32 mt-8">
+            <button className="bg-green-700 rounded-full p-1 md:px-4 text-gray-200 h-12 w-full lg:h-8 flex text-center justify-center items-center">
               SHOP NOW <FaArrowRight className="inline sm:mr-2 ml-8" />
             </button>
             <div className="stats text-green-700 flex flex-wrap justify-center sm:size-2 size-auto  md:size-auto flex-row mt-8 sm:mt-0 sm:flex-col md:flex-row ml-2 sm:ml-8 gap-8 ">
-              <div className="flex-shrink   border-r md:pr-4 sm:flex-col lg:border-r sm:border-0  border-gray-500">
-                <h1 className="font-bold flex ">35K +</h1>
+              <div className="flex-shrink border-r md:pr-4 sm:flex-col lg:border-r sm:border-0  border-gray-500">
+                <h1 className="font-bold flex pr-8 ">35K +</h1>
                 <p className="m-1 flex">Users</p>
               </div>
               <div className="flex-shrink sm:flex-col ">
@@ -35,11 +35,11 @@ function Hero() {
                 <p className="m-1 flex">Products</p>
               </div>
             </div>
+            <HeroImage />
           </div>
         </div>
       </div>
       <div className="rightContainer px-16 py-4 bg-green-800  flex flex-row sm:w-5/12 sm:h-[600px]  h-32  justify-between content-center"></div>
-      <HeroImage />
     </div>
   )
 }
--- src/components/hero/HeroImage.jsx
+++ src/components/hero/HeroImage.jsx
@@ -3,9 +3,8 @@ function HeroImage() {
     <img
       src="./fotor.png"
       alt="hero image"
-      className="absolute  object-cover object-center z-2  
-                rounded-full bg-center flex flex-shrink  left-1/2 transform -translate-x-1/2 sm:translate-x-0 sm:transform-none  sm:left-2/4 sm:top-1/4 size-48 
-                 lg:size-64   sm:size-64 sm:bottom-8 top-[44rem]"
+      className="sm:absolute  sm:object-cover sm:object-center relative z-1  -bottom-20 rounded-full sm:bg-center flex flex-shrink   sm:translate-x-0 sm:transform-none  sm:left-2/4 sm:top-1/4 size-48 
+                 lg:size-64   sm:size-64 sm:bottom-8"
     />
   )
 }
